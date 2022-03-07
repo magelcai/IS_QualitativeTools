daf362d28fdf61d0265d42a09275cbab77b0fdcb
magelcai <whitecai@oregonstate.edu>
2022-03-07 14:42:43 -0800

    updated distance calcs for 2022 topologies

--- DistanceCalcs.R
+++ DistanceCalcs.R
@@ -1,22 +1,23 @@
 #Calculate Network Metrics (Distance and Density) using iGraph
 
-install.packages("igraph")
+#install.packages("igraph")
 library(igraph)
 library(here)
 library(QPress)
 
 ##### Load the Dia models #####
-statquo <- QPress::model.dia("./DiaModels/MultiMods/StatusQuo_3Sept2021_forR.dia")
-mod_redevB_newdevC <- QPress::model.dia("./DiaModels/MultiMods/Moderate_RedevB+NewDevC_3Sept2021_forR.dia") 
+statquo <- QPress::model.dia("./DiaModels/MultiMods_Jan2022/StatusQuo_Jan2022_forR.dia")
+moderate <- QPress::model.dia("./DiaModels/MultiMods_Jan2022/Moderate_Jan2022_forR.dia") 
 
-redev_A <- QPress::model.dia("./DiaModels/MultiMods/RedevA_3Sept2021_forR.dia")
-redev_B <- QPress::model.dia("./DiaModels/MultiMods/RedevB_3Sept2021_forR.dia") 
-redev_C <- QPress::model.dia("./DiaModels/MultiMods/RedevC_3Sept2021_forR.dia") 
+newdev_A <- QPress::model.dia("./DiaModels/MultiMods_Jan2022/NewDevA_Jan2022_forR.dia") 
+newdev_B <- QPress::model.dia("./DiaModels/MultiMods_Jan2022/NewDevB_Jan2022_forR.dia") 
+newdev_C <- QPress::model.dia("./DiaModels/MultiMods_Jan2022/NewDevC_Jan2022_forR.dia") 
+newdev_D <- QPress::model.dia("./DiaModels/MultiMods_Jan2022/NewDevD_Jan2022_forR.dia") 
 
-newdev_A <- QPress::model.dia("./DiaModels/MultiMods/NewDevA_3Sept2021_forR.dia") 
-newdev_B <- QPress::model.dia("./DiaModels/MultiMods/NewDevB_3Sept2021_forR.dia") 
-newdev_C <- QPress::model.dia("./DiaModels/MultiMods/NewDevC_3Sept2021_forR.dia") 
-newdev_D <- QPress::model.dia("./DiaModels/MultiMods/NewDevD_3Sept2021_forR.dia") 
+redev_E <- QPress::model.dia("./DiaModels/MultiMods_Jan2022/RedevE_Jan2022_forR.dia")
+redev_F <- QPress::model.dia("./DiaModels/MultiMods_Jan2022/RedevF_Jan2022_forR.dia") 
+redev_G <- QPress::model.dia("./DiaModels/MultiMods_Jan2022/RedevG_Jan2022_forR.dia") 
+redev_H <- QPress::model.dia("./DiaModels/MultiMods_Jan2022/RedevH_Jan2022_forR.dia") 
  
 
 ##### Adjacency Matrix #####
@@ -35,16 +36,17 @@ create_adj = function (diamod)
 }
 
 ig_statquo <- create_adj(diamod = statquo)
-ig_mod <- create_adj(diamod = mod_redevB_newdevC)
-
-ig_redevA <- create_adj(diamod = redev_A)
-ig_redevB <- create_adj(diamod = redev_B)
-ig_redevC <- create_adj(diamod = redev_C)
+ig_mod <- create_adj(diamod = moderate)
 
 ig_newdevA <- create_adj(diamod = newdev_A)
 ig_newdevB <- create_adj(diamod = newdev_B)
 ig_newdevC <- create_adj(diamod = newdev_C)
 ig_newdevD <- create_adj(diamod = newdev_D)
+
+ig_redevE <- create_adj(diamod = redev_E)
+ig_redevF <- create_adj(diamod = redev_F)
+ig_redevG <- create_adj(diamod = redev_G)
+ig_redevH <- create_adj(diamod = redev_H)
 
 
 #Examine the igraph plots of the models
@@ -52,17 +54,19 @@ plot.igraph(ig_statquo) #basic plot
 tkplot(ig_statquo) #fancy plot that can be manipulated
 
 #Calculate Edge Density: The density of a graph is the ratio of the number of edges and the number of possible edges.
+#For Table S3
 edge_density(ig_statquo)
 edge_density(ig_mod)
-
-edge_density(ig_redevA)
-edge_density(ig_redevB)
-edge_density(ig_redevC)
 
 edge_density(ig_newdevA)
 edge_density(ig_newdevB)
 edge_density(ig_newdevC)
 edge_density(ig_newdevD)
+
+edge_density(ig_redevE)
+edge_density(ig_redevF)
+edge_density(ig_redevG)
+edge_density(ig_redevH)
 
 #distance_table returns a named list with two entries: res is a numeric vector, the histogram of distances, unconnected is a numeric scalar, 
 #the number of pairs for which the first vertex is not reachable from the second. The sum of the two entries is always n(n-1) 
@@ -71,18 +75,18 @@ dist_tab <- distance_table(ig_statquo)
 
 #mean_distance calculates the average path length in a graph, by calculating the shortest paths between all pairs of vertices 
 #(both ways for directed graphs). This function does not consider edge weights currently and uses a breadth-first search.
-mean_distance(ig_statquo) #2.41
-mean_distance(ig_mod) #2.39645
+mean_distance(ig_statquo) #2.414201
+mean_distance(ig_mod) #2.418146
 
-mean_distance(ig_redevA) #2.394841
-mean_distance(ig_redevB) #2.392505
-mean_distance(ig_redevC) #2.39
+mean_distance(ig_newdevA) #2.406312
+mean_distance(ig_newdevB) #2.402367
+mean_distance(ig_newdevC) #2.418146
+mean_distance(ig_newdevD) #2.421471
 
-mean_distance(ig_newdevA)
-mean_distance(ig_newdevB)
-mean_distance(ig_newdevC)
-mean_distance(ig_newdevD)
-
+mean_distance(ig_redevE) #2.414201
+mean_distance(ig_redevF) #2.414201
+mean_distance(ig_redevG) #2.392505
+mean_distance(ig_redevH) #2.416016
 
 #distances calculates the lengths of pairwise shortest paths from a set of vertices (from) to another set of vertices (to).
 NewDevDist_statquo <- as.data.frame(t(distances(ig_statquo, 16, mode = "out")))
@@ -101,14 +105,16 @@ rownames(NewDev_dists) <- nodes[c(1:33)]
 
 ReDevDist_statquo <- as.data.frame(distances(ig_statquo, 21, mode = "out"))
 ReDevDist_mod <- as.data.frame(distances(ig_mod, 21, mode = "out"))
-ReDevDist_newdevA <- as.data.frame(distances(ig_redevA, 21, mode = "out"))
-ReDevDist_newdevB <- as.data.frame(distances(ig_redevB, 21, mode = "out"))
-ReDevDist_newdevC <- as.data.frame(distances(ig_redevC, 21, mode = "out"))
+ReDevDist_redevE <- as.data.frame(distances(ig_redevE, 21, mode = "out"))
+ReDevDist_redevF <- as.data.frame(distances(ig_redevF, 21, mode = "out"))
+ReDevDist_redevG <- as.data.frame(distances(ig_redevG, 21, mode = "out"))
+ReDevDist_redevH <- as.data.frame(distances(ig_redevH, 21, mode = "out"))
 
-ReDev_list <- list("ReDevDist_statquo", "ReDevDist_mod","ReDevDist_newdevA", "ReDevDist_newdevB", "ReDevDist_newdevC")
 
-ReDev_dists <- do.call(rbind.data.frame, Map(data.frame, ReDevstatquo=ReDevDist_statquo, ReDevmod = ReDevDist_mod, ReDevA=ReDevDist_newdevA, ReDevB=ReDevDist_newdevB, 
-                                             ReDevC=ReDevDist_newdevC))
+ReDev_list <- list("ReDevDist_statquo", "ReDevDist_mod","ReDevDist_redevE", "ReDevDist_redevF", "ReDevDist_redevG", "ReDevDist_redevH")
+
+ReDev_dists <- do.call(rbind.data.frame, Map(data.frame, ReD_statquo=ReDevDist_statquo, ReD_mod = ReDevDist_mod, ReD_E=ReDevDist_redevE, ReD_F=ReDevDist_redevF, 
+                                             ReD_G=ReDevDist_redevG, ReD_H=ReDevDist_redevH))
 nodes <- QPress::node.labels(statquo)
 rownames(ReDev_dists) <- nodes[c(1:33)]
 
